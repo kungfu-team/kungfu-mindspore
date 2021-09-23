e80e4f3c47b5e83926078d1d1462f6ee64158635
lg <lg4869@outlook.com>
2021-09-23 18:12:59 +0000

    plot

--- experiments/midterm/scaling-convergence/plot_convergence.py
+++ experiments/midterm/scaling-convergence/plot_convergence.py
@@ -1,14 +1,15 @@
+#!/usr/bin/env python3
+
 import os
 import re
-​
-​
+
+
 def main():
-    directory = os.path.join(os.path.expanduser("~"),
-                             "Elasticity/Guo/scaling-convergence/log")
+    directory = os.path.join("log")
     loss_filename = "loss.csv"
     with open(loss_filename, "w") as loss_file:
         loss_file.write("rank,epoch,step,loss\n")
-​
+
         for filename in os.scandir(directory):
             if filename.is_file():
                 if "stdout.log" in filename.path:
@@ -19,13 +20,16 @@ def main():
                             m = re.match("^kungfu rank=*(\d)", line)
                             if m:
                                 rank = int(m.group(1))
-                            m = re.match("^epoch: *(\d+) step: *(\d+), loss is *(\d[.]\d+)", line)
+                            m = re.match(
+                                "^epoch: *(\d+) step: *(\d+), loss is *(\d[.]\d+)",
+                                line)
                             if m:
                                 epoch = int(m.group(1))
                                 step = int(m.group(2))
                                 loss = float(m.group(3))
-                                loss_file.write("{},{},{},{}\n".format(rank,epoch,step,loss))
-​
-​
+                                loss_file.write("{},{},{},{}\n".format(
+                                    rank, epoch, step, loss))
+
+
 if __name__ == "__main__":
     main()
